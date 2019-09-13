cf5e885235a3777d646f9586537aa1417d4b39a6
Doni Setiawan <donisetiawan.050@gmail.com>
2019-09-13 16:28:19 +0700

    Redirect component

---
+++ app/containers/AboutPage/__tests__/redirect-test.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import RedirectDemo from '../redirect';
+
+jest.mock('react-router-dom', () => ({
+  Redirect: 'Redirect',
+  Route: 'Route',
+}));
+
+it('should render correctly', () => {
+  const component = renderer.create(<RedirectDemo />);
+  expect(component.toJSON()).toMatchSnapshot();
+});
---
+++ app/containers/AboutPage/redirect.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Route, Redirect } from 'react-router-dom';
+
+const redirect = () => (
+  <Route
+    exact
+    path="/"
+    render={() => (loggedIn ? <Redirect to="/dashboard" /> : <PublicHomePage />)}
+  />
+);
+
+export default redirect;
