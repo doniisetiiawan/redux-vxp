cc59c91183ef9a8857937fb074c82739594dfa07
Doni Setiawan <donisetiawan.050@gmail.com>
2019-09-13 16:22:25 +0700

    ...

--- app/containers/HomePage/index.jsx
+++ app/containers/HomePage/index.jsx
@@ -1,7 +1,26 @@
-import React, { PureComponent } from 'react';
+import React from 'react';
+import { Route, Link } from 'react-router-dom';
 
-export default class HomePage extends PureComponent {
-  render() {
-    return <h1>This is the HomePage Redux-book container!</h1>;
-  }
-}
+const MenuLink = ({ to, ...rest }) => (
+  <Route path={to}>
+    {({ match }) => (
+      <li className={match ? 'active' : ''}>
+        <Link to={to} {...rest} />
+      </li>
+    )}
+  </Route>
+);
+
+const Homepage = () => (
+  <div>
+    <h1>This is home page our our application.</h1>
+    <ul>
+      <MenuLink to="/about">About Us</MenuLink>
+      <MenuLink to="/login">Login</MenuLink>
+      <MenuLink to="/register">Register</MenuLink>
+      <MenuLink to="/contact">Contact</MenuLink>
+    </ul>
+  </div>
+);
+
+export default Homepage;
