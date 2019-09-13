57160a0efd85e66194bc7c977e619e3f09c30bf0
Doni Setiawan <donisetiawan.050@gmail.com>
2019-09-13 15:01:44 +0700

    Testing redux

---
+++ app/Redux/AlertContainer.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+import Alerts from './Alerts';
+
+class AlertContainer extends Component {
+  componentDidMount() {
+    const { dispatch } = this.props;
+    dispatch({ type: 'REQUEST_ALERTS_LISTS' });
+  }
+
+  render() {
+    return <Alerts />;
+  }
+}
+
+export default connect()(AlertContainer);
