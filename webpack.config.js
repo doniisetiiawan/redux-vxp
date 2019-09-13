ed4ecbcaaaab3890b62bee915feff9ac5ec28306
Doni Setiawan <donisetiawan.050@gmail.com>
2019-09-13 10:07:16 +0700

    Getting started

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/app/app.js',
+  output: {
+    path: path.resolve('dist'),
+    filename: 'main.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        use: {
+          loader: 'babel-loader',
+        },
+        exclude: /(node_modules|bower_components)/,
+      },
+    ],
+  },
+};
