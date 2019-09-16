3e90b73c8f665b2062aa8f0e0f2bc9930db97aaf
Doni Setiawan <donisetiawan.050@gmail.com>
2019-09-16 12:12:09 +0700

    Getting started redux-saga

--- app/configureStore.js
+++ app/configureStore.js
@@ -1,11 +1,13 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import { fromJS } from 'immutable';
 import { routerMiddleware } from 'connected-react-router/immutable';
-
+import createSagaMiddleware from 'redux-saga';
 import createReducer from './reducers';
 
+const sagaMiddleware = createSagaMiddleware();
+
 export default function configureStore(initialState = {}, history) {
-  const middlewares = [routerMiddleware(history)];
+  const middlewares = [sagaMiddleware, routerMiddleware(history)];
 
   const enhancers = [applyMiddleware(...middlewares)];
 
@@ -15,7 +17,9 @@ export default function configureStore(initialState = {}, history) {
     compose(...enhancers),
   );
 
+  store.runSaga = sagaMiddleware.run;
   store.injectedReducers = {};
+  store.injectedSagas = {};
 
   return store;
 }
